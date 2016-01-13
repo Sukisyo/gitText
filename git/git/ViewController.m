5434caa4c158c3a56b366ffdd8bd924406748569
Sukisyo <1196378539@qq.com>
2016-01-13 14:17:50 +0800

    第一次修改

--- git/git/ViewController.m
+++ git/git/ViewController.m
@@ -17,6 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSLog(@"真是日了狗了");
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
